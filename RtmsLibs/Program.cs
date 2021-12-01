9ecb6d4a8359d08d7b087b13a1ed127948c57337
ivkinp <ivkin@ukr.net>
2021-12-01 22:24:29 +0200

    addint good bye world

--- RtmsLibs/Program.cs
+++ RtmsLibs/Program.cs
@@ -11,6 +11,7 @@ namespace RtmsLibs
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Good Bye World");
         }
     }
 }
